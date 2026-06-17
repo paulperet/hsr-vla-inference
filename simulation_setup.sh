ba0fa6cdf82b5824fd37ce73a5422fcc43efb16a
Paul Péret <40545018+paulperet@users.noreply.github.com>
2026-06-17 22:47:49 +0100

    fix: container absolute path

--- simulation_setup.sh
+++ simulation_setup.sh
@@ -15,8 +15,8 @@ source /root/catkin_ws/devel/setup.bash && catkin_make
 # Create ROS node script
 mkdir /root/catkin_ws/src/hsr_controller/scripts
 
-cp hsr_debug.py /root/catkin_ws/src/hsr_controller/scripts
-cp hsr_controller.py /root/catkin_ws/src/hsr_controller/scripts
+cp /home/developer/hsr-vla-inference/hsr_debug.py /root/catkin_ws/src/hsr_controller/scripts
+cp /home/developer/hsr-vla-inference/hsr_controller.py /root/catkin_ws/src/hsr_controller/scripts
 
 chmod +x /root/catkin_ws/src/hsr_controller/scripts/hsr_debug.py
 chmod +x /root/catkin_ws/src/hsr_controller/scripts/hsr_controller.py
